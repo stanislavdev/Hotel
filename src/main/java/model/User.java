a5d7eed4adddb6e8c3c609e7a3d8dfb3edd0a658
stanislavdev <dvstas00@gmail.com>
2017-12-24 17:51:32 +0200

    Add UserBuilder

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -8,6 +8,13 @@ public class User {
     private String password;
     private Role role;
 
+    private User(UserBuilder userBuilder) {
+        id = userBuilder.id;
+        email = userBuilder.email;
+        password = userBuilder.password;
+        role = userBuilder.role;
+    }
+
     public int getId() {
         return id;
     }
@@ -55,4 +62,37 @@ public class User {
     public int hashCode() {
         return Objects.hash(id, email, password, role);
     }
+
+    public static class UserBuilder {
+        private int id;
+        private String email;
+        private String password;
+        private Role role;
+
+        public UserBuilder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder role(String role) {
+            this.role = Role.valueOf(role);
+            return this;
+        }
+
+        public User build(){
+           return new User(this);
+        }
+    }
+
+
 }
