931202797328a7f628a7f03d69e6e1cb60b92e3a
stanislavdev <dvstas00@gmail.com>
2018-01-12 01:17:14 +0200

    add method for getting all orders

--- src/main/java/model/services/OrderService.java
+++ src/main/java/model/services/OrderService.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface OrderService {
     void createOrder(User user, Date dateFrom, Date dateTo, ApartmentType apartmentType, int numberOfRooms);
     List<Order> showUserOrders(User user);
+    List<Order> getAllOrders();
 }
