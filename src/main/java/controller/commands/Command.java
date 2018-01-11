a3518b25dc87a085e572aa80d04e7e4f8963b0dd
stanislavdev <dvstas00@gmail.com>
2018-01-12 00:05:44 +0200

    refactor constants

--- src/main/java/controller/commands/Command.java
+++ src/main/java/controller/commands/Command.java
@@ -5,8 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface Command {
     String SIGN_IN_PAGE = "/sign_in.jsp";
-    String CLIENT_HOME = "/client/home";
-    String ADMIN_HOME = "/admin/home";
+    String CLIENT_HOME_PAGE = "/client_home.jsp";
+    String ADMIN_HOME_PAGE = "/admin_home.jsp";
+    String REGISTRATION_PAGE = "/registration_page.jsp";
 
-    String execute(HttpServletRequest httpRequest, HttpServletResponse httpResponse);
+    String execute(HttpServletRequest request, HttpServletResponse response);
 }
