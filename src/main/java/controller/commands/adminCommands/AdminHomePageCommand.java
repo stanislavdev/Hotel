0d602f5044e3f5e7882c9f7ede69af589e827441
stanislavdev <dvstas00@gmail.com>
2018-01-12 01:52:46 +0200

    add order service object

--- src/main/java/controller/commands/adminCommands/AdminHomePageCommand.java
+++ src/main/java/controller/commands/adminCommands/AdminHomePageCommand.java
@@ -1,13 +1,20 @@
 package controller.commands.adminCommands;
 
 import controller.commands.Command;
+import model.entities.Order;
+import model.services.OrderService;
+import model.services.impl.OrderServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class AdminHomePageCommand implements Command {
     @Override
-    public String execute(HttpServletRequest httpRequest, HttpServletResponse httpResponse) {
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        OrderService orderService = new OrderServiceImpl();
+        List<Order> orders = orderService.getAllOrders();
+        request.getSession().setAttribute("orders", orders);
         return ADMIN_HOME_PAGE;
     }
 }
