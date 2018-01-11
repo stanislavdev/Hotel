63548b6f7e346ddd93007e4bb097818e92f664e7
stanislavdev <dvstas00@gmail.com>
2018-01-11 23:53:33 +0200

    add throw SQLExeption

--- src/main/java/model/services/AccountService.java
+++ src/main/java/model/services/AccountService.java
@@ -2,10 +2,11 @@ package model.services;
 
 import model.entities.User;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface AccountService {
     Optional<User> signIn(String email, String password);
 
-    void signUp(User user);
+    void signUp(User user) throws SQLException;
 }
