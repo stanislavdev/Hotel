0da9c9fa718438b793146038216de63d690919f4
stanislavdev <dvstas00@gmail.com>
2018-01-11 23:57:05 +0200

    add client page command

---
+++ src/main/java/controller/commands/clientCommands/ClientPageCommand.java
@@ -0,0 +1,22 @@
+package controller.commands.clientCommands;
+
+import controller.commands.Command;
+import model.entities.Order;
+import model.entities.User;
+import model.services.OrderService;
+import model.services.impl.OrderServiceImpl;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public class ClientPageCommand implements Command {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        User user = (User) request.getSession().getAttribute("user");
+        OrderService orderService = new OrderServiceImpl();
+        List<Order> orders = orderService.showUserOrders(user);
+        request.getSession().setAttribute("orders", orders);
+        return CLIENT_HOME_PAGE;
+    }
+}
