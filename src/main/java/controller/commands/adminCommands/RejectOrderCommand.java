5071a716620724b60e9343299a3a49e9acb97694
stanislavdev <dvstas00@gmail.com>
2018-01-22 01:56:07 +0200

    add ability to reject order by admin

---
+++ src/main/java/controller/commands/adminCommands/RejectOrderCommand.java
@@ -0,0 +1,27 @@
+package controller.commands.adminCommands;
+
+import controller.commands.Command;
+import controller.commands.CommandFactory;
+import model.services.OrderService;
+import model.services.impl.OrderServiceImpl;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static model.util.Constants.ORDER_ID_ATTRIBUTE;
+import static model.util.Constants.USER_ID_ATTRIBUTE;
+
+public class RejectOrderCommand implements Command {
+    private OrderService orderService = OrderServiceImpl.getInstance();
+
+    private static final Logger LOGGER = Logger.getLogger(RejectOrderCommand.class);
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        String orderId = String.valueOf(request.getSession().getAttribute(ORDER_ID_ATTRIBUTE));
+        orderService.rejectOrderById(Integer.parseInt(orderId));
+        LOGGER.info("Admin " + request.getSession().getAttribute(USER_ID_ATTRIBUTE) + " reject order " + orderId);
+        return "redirect:" + CommandFactory.ADMIN_HOME_PAGE;
+    }
+}
