e2a0299c8a5ede57156381a598e64f5bf2ba43da
stanislavdev <dvstas00@gmail.com>
2018-01-22 02:08:31 +0200

    add several methods

--- src/main/java/model/services/OrderService.java
+++ src/main/java/model/services/OrderService.java
@@ -13,8 +13,12 @@ public interface OrderService {
     void createOrder(int userId, Date dateFrom, Date dateTo, ApartmentType apartmentType, String numberOfRooms);
     List<Order> getAllUserOrders(int userId);
     List<Order> getAllOrders();
+    List<Order> getAllLimitedOrdersByUserId(int start, int total, int userId);
     List<Order> getAllLimitedOrders(int start, int total);
     Optional<Order> getById(int id);
     void updateToAccepted(Order order);
-    void insertIntoOrderHasApartments(int orderId, int apartmentId) throws SQLException;
+    void insertIntoOrderHasApartments(int orderId, int apartmentId);
+    int getTotalNumberOfOrders();
+    int getNumberOfOrdersByClientId(int id);
+    void rejectOrderById(int id);
 }
--- src/main/java/model/services/impl/OrderServiceImpl.java
+++ src/main/java/model/services/impl/OrderServiceImpl.java
@@ -2,12 +2,10 @@ package model.services.impl;
 
 import model.dao.FactoryDAO;
 import model.dao.OrderDAO;
-import model.dao.impl.MySQLFactoryDAO;
-import model.dao.impl.MySQLOrderDAO;
 import model.entities.ApartmentType;
 import model.entities.Order;
-import model.entities.User;
 import model.services.OrderService;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.Date;
@@ -17,6 +15,8 @@ import java.util.Optional;
 
 public class OrderServiceImpl implements OrderService {
     private FactoryDAO factoryDAO;
+
+    private static final Logger LOGGER = Logger.getLogger(OrderServiceImpl.class);
 
     private static class Holder {
         private static OrderServiceImpl INSTANCE = new OrderServiceImpl();
@@ -63,9 +63,17 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    public List<Order> getAllLimitedOrdersByUserId(int start, int total, int userId) {
+        OrderDAO orderDAO = factoryDAO.getOrderDAO();
+        List<Order> orderList = orderDAO.getAllLimitedOrdersByUserId(start, total, userId);
+        orderDAO.close();
+        return orderList;
+    }
+
+    @Override
     public List<Order> getAllLimitedOrders(int start, int total) {
         OrderDAO orderDAO = factoryDAO.getOrderDAO();
-        List<Order> orderList = orderDAO.getAllLimit(start, total);
+        List<Order> orderList = orderDAO.getAllLimitedOrders(start, total);
         orderDAO.close();
         return orderList;
     }
@@ -86,7 +94,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
-    public void insertIntoOrderHasApartments(int orderId, int apartmentId) throws SQLException {
+    public void insertIntoOrderHasApartments(int orderId, int apartmentId) {
         try (Connection connection = factoryDAO.getConnection()) {
             connection.setAutoCommit(false);
             OrderDAO orderDAO = factoryDAO.getOrderDAO();
@@ -95,6 +103,32 @@ public class OrderServiceImpl implements OrderService {
             } else {
                 connection.rollback();
             }
+        } catch (SQLException e) {
+            LOGGER.error(e);
+            throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public int getTotalNumberOfOrders() {
+        OrderDAO orderDAO = factoryDAO.getOrderDAO();
+        int result = orderDAO.getNumberOfOrders();
+        orderDAO.close();
+        return result;
+    }
+
+    @Override
+    public int getNumberOfOrdersByClientId(int id) {
+        OrderDAO orderDAO = factoryDAO.getOrderDAO();
+        int result = orderDAO.getNumberOfOrdersByClientId(id);
+        orderDAO.close();
+        return result;
+    }
+
+    @Override
+    public void rejectOrderById(int id) {
+        OrderDAO orderDAO = factoryDAO.getOrderDAO();
+        orderDAO.rejectOrderById(id);
+        orderDAO.close();
     }
 }
