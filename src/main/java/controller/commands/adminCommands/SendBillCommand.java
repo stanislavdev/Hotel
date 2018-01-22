9608bfb4168bf7c4a6b8a99d8ee6f87341884383
stanislavdev <dvstas00@gmail.com>
2018-01-22 02:09:21 +0200

    code refactor

--- src/main/java/controller/commands/adminCommands/SendBillCommand.java
+++ src/main/java/controller/commands/adminCommands/SendBillCommand.java
@@ -2,15 +2,19 @@ package controller.commands.adminCommands;
 
 import controller.commands.Command;
 import controller.commands.CommandFactory;
+import model.entities.Apartment;
 import model.entities.Bill;
 import model.entities.Order;
 import model.entities.User;
 import model.services.ApartmentService;
 import model.services.BillService;
 import model.services.OrderService;
+import model.services.UserService;
 import model.services.impl.ApartmentServiceImpl;
 import model.services.impl.BillServiceImpl;
 import model.services.impl.OrderServiceImpl;
+import model.services.impl.UserServiceImpl;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,33 +23,60 @@ import java.sql.SQLException;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
+import static model.util.Constants.APARTMENT_ID_ATTRIBUTE;
+import static model.util.Constants.ORDER_ID_ATTRIBUTE;
+import static model.util.Constants.USER_ID_ATTRIBUTE;
+
 public class SendBillCommand implements Command {
+    private OrderService orderService = OrderServiceImpl.getInstance();
+    private ApartmentService apartmentService = ApartmentServiceImpl.getInstance();
+    private BillService billService = BillServiceImpl.getInstance();
+    private UserService userService = UserServiceImpl.getInstance();
+
+    private String apartmentId;
+    private String orderId;
+    private Order order;
+    private Bill bill;
+    private int totalPrice;
+
+    private static final Logger LOGGER = Logger.getLogger(SendBillCommand.class);
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        String apartmentId = request.getParameter("chosenApartment");
-        String orderId = String.valueOf(request.getSession().getAttribute("orderId"));
-        OrderService orderService = new OrderServiceImpl();
-        Optional<Order> order = orderService.getById(Integer.parseInt(orderId));
-        Date dateFrom = order.get().getDateFrom();
-        Date dateTo = order.get().getDateTo();
-        ApartmentService apartmentService = new ApartmentServiceImpl();
+        init(request);
+        countOrderPrice();
+        createBill(request);
+        orderService.updateToAccepted(order);
+        orderService.insertIntoOrderHasApartments(Integer.parseInt(orderId), Integer.parseInt(apartmentId));
+        billService.createNewBill(bill);
+        LOGGER.info("Admin " + request.getSession().getAttribute(USER_ID_ATTRIBUTE) +
+                " send bill for order " + orderId);
+        return "redirect:" + CommandFactory.ADMIN_HOME_PAGE;
+    }
+
+    private void init(HttpServletRequest request) {
+        apartmentId = request.getParameter(APARTMENT_ID_ATTRIBUTE);
+        orderId = String.valueOf(request.getSession().getAttribute(ORDER_ID_ATTRIBUTE));
+        order = orderService.getById(Integer.parseInt(orderId)).get();
+    }
+
+    private void countOrderPrice() {
+        Apartment apartment = apartmentService.getById(Integer.parseInt(apartmentId)).get();
+        Date dateFrom = order.getDateFrom();
+        Date dateTo = order.getDateTo();
         long days = dateTo.getTime() - dateFrom.getTime();
         long convertDays = TimeUnit.DAYS.convert(days, TimeUnit.MILLISECONDS);
-        int price = Math.toIntExact(convertDays * (apartmentService.getById(Integer.parseInt(apartmentId)).get().getPrice()));
-        BillService billService = new BillServiceImpl();
-        Bill bill = new Bill.BillBuilder()
-                .admin((User) request.getSession().getAttribute("user"))
-                .order(order.get())
-                .price(price)
+        int pricePerDay = (apartment.getPrice());
+        totalPrice = Math.toIntExact(convertDays * pricePerDay);
+    }
+
+    private void createBill(HttpServletRequest request) {
+        int userId = (int) request.getSession().getAttribute(USER_ID_ATTRIBUTE);
+        User user = userService.getById(userId).get();
+        bill = new Bill.BillBuilder()
+                .admin(user)
+                .order(order)
+                .price(totalPrice)
                 .build();
-        orderService.updateToAccepted(order.get());
-        try {
-            orderService.insertIntoOrderHasApartments(Integer.parseInt(orderId), Integer.parseInt(apartmentId));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        billService.createNewBill(bill);
-        return "redirect:" + CommandFactory.ADMIN_HOME_PAGE;
     }
 }
