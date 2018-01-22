6b3b84ac1e6b58a1276522e07daf977a9dbbee1b
stanislavdev <dvstas00@gmail.com>
2018-01-22 02:09:45 +0200

    some code refactor

--- src/main/java/controller/commands/adminCommands/ShowApartmentsForOrderCommand.java
+++ src/main/java/controller/commands/adminCommands/ShowApartmentsForOrderCommand.java
@@ -4,36 +4,38 @@ import controller.commands.Command;
 import controller.commands.CommandFactory;
 import model.entities.Apartment;
 import model.entities.Order;
-import model.entities.User;
-import model.services.UserService;
 import model.services.ApartmentService;
 import model.services.OrderService;
-import model.services.impl.UserServiceImpl;
 import model.services.impl.ApartmentServiceImpl;
 import model.services.impl.OrderServiceImpl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.Optional;
 
 import static model.util.Constants.*;
 
-public class ChoseApartmentForOrderCommand implements Command {
+public class ShowApartmentsForOrderCommand implements Command {
     private ApartmentService apartmentService = ApartmentServiceImpl.getInstance();
     private OrderService orderService = OrderServiceImpl.getInstance();
 
+    private String orderId;
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        String orderId = request.getParameter(APARTMENT_RADIO_ATTRIBUTE);
+        orderId = request.getParameter(ORDER_ID_ATTRIBUTE);
         if (orderId == null) {
             request.setAttribute(EXCEPTION_ATTRIBUTE, EXCEPTION_ATTRIBUTE);
             return "redirect:" + CommandFactory.ADMIN_HOME_PAGE;
         }
-        request.getSession().setAttribute(ORDER_ID_ATTRIBUTE, orderId);
-        Optional<Order> order = orderService.getById(Integer.parseInt(orderId));
-        List<Apartment> apartmentList = apartmentService.showAvailableApartments(order.get());
-        request.getSession().setAttribute(APARTMENTS_ATTRIBUTE, apartmentList);
+        setApartmentAttributes(request);
         return APARTMENTS_FOR_ORDER;
+    }
+
+    private void setApartmentAttributes(HttpServletRequest request) {
+        Order order = orderService.getById(Integer.parseInt(orderId)).get();
+        List<Apartment> apartmentList = apartmentService.showAvailableApartments(order);
+        request.getSession().setAttribute(ORDER_ID_ATTRIBUTE, orderId);
+        request.getSession().setAttribute(APARTMENTS_ATTRIBUTE, apartmentList);
     }
 }
