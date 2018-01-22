c5c85417e599500e8851849bf19695008d46505d
stanislavdev <dvstas00@gmail.com>
2018-01-22 02:06:04 +0200

    some refactor

--- src/main/java/controller/commands/Command.java
+++ src/main/java/controller/commands/Command.java
@@ -4,11 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Command {
-    String SIGN_IN_JSP = "/sign_in.jsp";
-    String CLIENT_HOME_JSP = "/client_home.jsp";
-    String ADMIN_HOME_JSP = "/admin_home.jsp";
+    String SIGN_IN_JSP = "/sign_in_page.jsp";
+    String CLIENT_HOME_JSP = "/client_home_page.jsp";
+    String ADMIN_HOME_JSP = "/admin_home_page.jsp";
     String REGISTRATION_PAGE_JSP = "/registration_page.jsp";
-    String APARTMENTS_FOR_ORDER = "/apartmentsForOrder.jsp";
+    String APARTMENTS_FOR_ORDER = "/apartments_selection.jsp";
     String CLIENT_BILLS_JSP = "/bills_page.jsp";
 
     String execute(HttpServletRequest request, HttpServletResponse response);
